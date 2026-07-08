33fc6851bf289cf3d46f4c7ebfff7e84ef0083ba
Yvvon Majala <149659748+YvvonM@users.noreply.github.com>
2026-07-08 13:03:49 +0000

    continued questions

--- C++_projects/answers.cpp
+++ C++_projects/answers.cpp
@@ -573,3 +573,26 @@ int main(){
 
 }
 
+/* 29.
+Create a function that returns the area of a triangle.
+area = (base * width)/2
+*/
+#include <iostream>
+using namespace std;
+
+double area(double base, int height){
+    return (base * height)/2;
+}
+
+int main(){
+    double base, height;
+    cout << "Enter base of the triangle: " << endl;
+    cin >> base;
+
+    cout << "Enter height of the triangle: " << endl;
+    cin >> height;
+
+    cout << "Area of the triangle is: "  << area(base, height);
+    return 0;
+
+}
